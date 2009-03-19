604a922fdcc4b28bbbb36ea6910d1af78ae2a6b6
Viola Holownia <violaholownia@viola-holownias-macbook.local>
2009-03-18 23:40:17 -0400

    add login module

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,15 +1,20 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+    before_filter :set_user
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '3b6d99b81d86ddcfd73a0b55f4b3feb4'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
-end
+  protected
+    def set_user
+      @user = User.find(session[:id]) if @user.nil? && session[:id]
+    end
+
+    def login_required
+      return true if @user
+      access_denied
+      return false
+    end
+
+    def access_denied
+      session[:return_to] = request.request_uri
+      flash[:error] = 'Oops. You need to login before you can view that page.'
+      redirect_to :controller => 'user', :action => 'login'
+    end
+end 
