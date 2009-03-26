9fa489f41afba39d98b5e66e9a88d0f9f2c2f068
azreal <michal.kuklis@gmail.com>
2009-03-25 21:41:39 -0400

    added cards scaffold

---
+++ db/migrate/20090326012024_create_cards.rb
@@ -0,0 +1,15 @@
+class CreateCards < ActiveRecord::Migration
+  def self.up
+    create_table :cards do |t|
+      t.string :name
+      t.text :description
+      t.string :type
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :cards
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090319031933) do
+ActiveRecord::Schema.define(:version => 20090326012024) do
+
+  create_table "cards", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
